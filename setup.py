a2b7f564990eb619ac9957e366a83b64941ef9ce
mingruimingrui <mingrui.wang@shopee.com>
2018-10-14 22:58:00 +0800

    added setup script to compile cpu_nms

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy as np
+
+setup(
+    include_dirs=[np.get_include()],
+    ext_modules=cythonize('./deep_look/utils/cpu_nms.pyx')
+)
